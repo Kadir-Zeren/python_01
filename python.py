75474f875849e367355749f061fd1fbb94d34f13
kadir-zeren <k4dzeren@gmail.com>
2024-05-04 11:16:07 +0300

    python intro

--- python.py
+++ python.py
@@ -1,1 +1,37 @@
 print("Hello world")
+print("Benim yazdigim ilk kod")
+print("Benim yazdigim ilk kod")
+print("Cift tirnak yazdim")
+print('''uc tirnak ile yazdim''')
+print("bir cumle")
+print("I'm great")
+print('"hi" he said')
+print('''"Don't do it" said the manager''')
+print('''bu birinci satir
+bu ikinci satir
+bu uc 
+bu dort''')
+print('3.14')
+print(3.14)
+print('3.14'*3)
+print(3.14*3)
+print('abcd'*2)
+print(5*2)
+print(''''We should have enough time for our family"''')
+print('bir')
+print()
+print('uc')
+print()
+print('')
+print()
+print("")
+print()
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print('kaskda;slkd ;laskd ;sakd;lsakd;la s \
+      asdl;asd;als;dlasdasd \
+      sadasdasdsa')
+print('kullanici ismi') #Burada kullanici ismi yazdiriliyor.
+x = 5 # burada x degeri atandi
